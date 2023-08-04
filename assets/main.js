0f570eea7584f45479baae7d33bf7835e9b32b19
CelesBariri <rafael.celes.gf@gmail.com>
2023-08-04 10:10:17 -0300

    Novas alterações

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+document.getElementById("btn").addEventListener("click", trocaNome );
+
+function trocaNome(e)
+{
+    let nome = document.getElementById( "nome" ).value
+    document.getElementById( "frase" ).innerHTML = "Meu nome é " + nome;
+    e.preventdefault();
+}
+function trocaCor( e ) {
+    document.body.style.backgroundColor= 'lightblue';
+}
